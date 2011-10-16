104f9d0dff25ace4e90b7d7067b7229a92157d0b
addyosmani <addyosmani@gmail.com>
2011-10-16 18:48:20 +0100

    updating structure

--- microtemplatez.js
+++ microtemplatez.js
@@ -3,18 +3,30 @@
 * Copyright: Addy Osmani, 2011.
 * Licensed under MIT/GPL licenses
 */
-function templatez(tmpl, data) {
-    return tmpl.replace((RegExp("{{\\s*([a-z0-9_][\\.a-z0-9_]*)\\s*}}", "gi")), function (tag, k) {
-        var p = k.split("."),
-            len = p.length,
-            temp = data,
-            i = 0;
-        for (; i < len; i++) {
-            temp = temp[p[i]];
-        }
-        return temp;
-    });
-};
+;( function ( window, doc, undef ) {
+
+    templatez = function( tmpl, data ) {
+        return tmpl.replace((RegExp("{{\\s*([a-z0-9_][\\.a-z0-9_]*)\\s*}}", "gi")), function (tag, k) {
+            var p = k.split("."),
+                len = p.length,
+                temp = data,
+                i = 0;
+            for (; i < len; i++) {
+                temp = temp[p[i]];
+            }
+            return temp;
+        });
+    };
+
+    getTemplatez = function(){
+        return templatez;
+    }
+
+    window.templatez = getTemplatez();
+
+} )( this, this.document );
+
+
 
 
 
